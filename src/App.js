d69399f2f16e59d70b81643872c3cbc93c9e1901
Himanshu Rishi <75842444+Himanshu-Rishi@users.noreply.github.com>
2022-11-24 22:20:35 +0530

    error solved before functin based component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import News from './components/News'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default class App extends Component {
-  pageSize = 5;
+  pageSize = 20;
   apikey = process.env.REACT_APP_API_KEY;
   render() {
     return (
--- src/components/News.js
+++ src/components/News.js
@@ -40,29 +40,9 @@ export class News extends Component {
   async componentDidMount() {
     this.updateNews();
   }
-  // handle_on_prev = async () => {
-  //   this.setState({
-  //     page: this.state.page - 1,
-  //   });
-  //   this.updateNews();
-  // };
-  // handle_on_next = async () => {
-  // if (
-  //   this.state.page + 1 >
-  //   Math.ceil(this.state.totalResults / this.props.pagesize)
-  // ) {
-  //   } else {
-  //     this.setState({
-  //       page: this.state.page + 1,
-  //     });
-  //     this.updateNews();
-  //   }
-  // };
-
   fetchMoreData = async () => {
-    console.log(this.state.page)
     this.setState({ page: this.state.page + 1 });
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d093053d72bc40248998159804e0e67d&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apikey}&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
@@ -77,7 +57,7 @@ export class News extends Component {
         <h1 className="text-center" style={{ margin: "35px 0px" }}>
           NewsMonkey - Top Headlines
         </h1>
-        {this.state.loading && <Spinner />}
+        {/* {this.state.loading && <Spinner />} */}
         <InfiniteScroll
           dataLength={this.state.articles.length}
           next={this.fetchMoreData}
@@ -112,27 +92,6 @@ export class News extends Component {
             </div>
           </div>
         </InfiniteScroll>
-        {/* <div className="d-flex justify-content-between my-3">
-          <button
-            type="button"
-            className="btn btn-primary"
-            onClick={this.handle_on_prev}
-            disabled={this.state.page <= 1}
-          >
-            Prev
-          </button>
-          <button
-            type="button"
-            className="btn btn-primary"
-            onClick={this.handle_on_next}
-            disabled={
-              this.state.page + 1 >
-              Math.ceil(this.state.totalResults / this.props.pagesize)
-            }
-          >
-            Next
-          </button>
-        </div> */}
       </>
     );
   }
