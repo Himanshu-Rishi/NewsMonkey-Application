6e7c35a4c20d27b553e7c725937920179342eb50
Himanshu Rishi <75842444+Himanshu-Rishi@users.noreply.github.com>
2022-11-24 22:12:07 +0530

    code before function based component

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-// import Spinner from "./Spinner";
+import Spinner from "./Spinner";
 import NewsItem from "./NewsItem";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
@@ -7,7 +7,7 @@ import InfiniteScroll from "react-infinite-scroll-component";
 export class News extends Component {
   static defaultProps = {
     country: "in",
-    pagesize: 12,
+    pagesize: 8,
     category: "general",
   };
   static propTypes = {
@@ -19,7 +19,7 @@ export class News extends Component {
     super();
     this.state = {
       articles: [],
-      loading: false,
+      loading: true,
       page: 1,
       totalResults: 0,
     };
@@ -40,64 +40,79 @@ export class News extends Component {
   async componentDidMount() {
     this.updateNews();
   }
-  handle_on_prev = async () => {
+  // handle_on_prev = async () => {
+  //   this.setState({
+  //     page: this.state.page - 1,
+  //   });
+  //   this.updateNews();
+  // };
+  // handle_on_next = async () => {
+  // if (
+  //   this.state.page + 1 >
+  //   Math.ceil(this.state.totalResults / this.props.pagesize)
+  // ) {
+  //   } else {
+  //     this.setState({
+  //       page: this.state.page + 1,
+  //     });
+  //     this.updateNews();
+  //   }
+  // };
+
+  fetchMoreData = async () => {
+    console.log(this.state.page)
+    this.setState({ page: this.state.page + 1 });
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d093053d72bc40248998159804e0e67d&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
     this.setState({
-      page: this.state.page - 1,
+      articles: this.state.articles.concat(parsedData.articles),
+      totalResults: parsedData.totalResults,
     });
-    this.updateNews();
   };
-  handle_on_next = async () => {
-    if (
-      this.state.page + 1 >
-      Math.ceil(this.state.totalResults / this.props.pagesize)
-    ) {
-    } else {
-      this.setState({
-        page: this.state.page + 1,
-      });
-      this.updateNews();
-    }
-  };
+
   render() {
     return (
-      <div className="container my-3">
+      <>
         <h1 className="text-center" style={{ margin: "35px 0px" }}>
           NewsMonkey - Top Headlines
         </h1>
-        {/* {this.state.loading && <Spinner />} */}
+        {this.state.loading && <Spinner />}
         <InfiniteScroll
           dataLength={this.state.articles.length}
           next={this.fetchMoreData}
-          hasMore={this.state.articles.length !== this.totalResults}
-          loader={<h4>Loading...</h4>}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner />}
         >
-          <div className="row">
-            {this.state.articles.map((element) => {
-              return (
-                <div className="col-md-4" key={element.url}>
-                  <NewsItem
-                    title={element.title ? element.title.slice(0, 45) : ""}
-                    description={
-                      element.description
-                        ? element.description.slice(0, 88)
-                        : ""
-                    }
-                    imageUrl={
-                      element.urlToImage
-                        ? element.urlToImage
-                        : "https://media.istockphoto.com/id/1197831888/vector/male-hand-holding-megaphone-with-breaking-news-speech-bubble-loudspeaker-banner-for-business.jpg?s=612x612&w=0&k=20&c=4CvdND_C8H3AxDMlEAZ8j9oagSvlmMcNHlrVoqoc9KQ="
-                    }
-                    newsUrl={element.url}
-                    author={element.author}
-                    date={element.publishedAt}
-                    source={element.source}
-                  />
-                </div>
-              );
-            })}
+          <div className="container">
+            <div className="row">
+              {this.state.articles.map((element, index) => {
+                return (
+                  <div className="col-md-4" key={index}>
+                    <NewsItem
+                      title={element.title ? element.title.slice(0, 45) : ""}
+                      description={
+                        element.description
+                          ? element.description.slice(0, 88)
+                          : ""
+                      }
+                      imageUrl={
+                        element.urlToImage
+                          ? element.urlToImage
+                          : "https://media.istockphoto.com/id/1197831888/vector/male-hand-holding-megaphone-with-breaking-news-speech-bubble-loudspeaker-banner-for-business.jpg?s=612x612&w=0&k=20&c=4CvdND_C8H3AxDMlEAZ8j9oagSvlmMcNHlrVoqoc9KQ="
+                      }
+                      newsUrl={element.url}
+                      author={element.author}
+                      date={element.publishedAt}
+                      source={element.source}
+                    />
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </InfiniteScroll>
-        <div className="d-flex justify-content-between my-3">
+        {/* <div className="d-flex justify-content-between my-3">
           <button
             type="button"
             className="btn btn-primary"
@@ -117,8 +132,8 @@ export class News extends Component {
           >
             Next
           </button>
-        </div>
-      </div>
+        </div> */}
+      </>
     );
   }
 }
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import loading from './loading.gif'
-export default class Spinner extends Component {
+export class Spinner extends Component {
   render() {
     return (
         <div className='text-center'>
-            <img src={loading} alt="" />
+            <img src={loading} alt="" className='my-3'/>
         </div>
     )
   } 
 }
+
+export default Spinner;
